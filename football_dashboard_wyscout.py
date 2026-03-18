8298c96fd542159f2617ef9ba0ae4074ab224740
JamieWol <jamiewolstenholme99@hotmail.com>
2026-03-18 19:28:14 +0000

    Update football_dashboard_wyscout.py

--- football_dashboard_wyscout.py
+++ football_dashboard_wyscout.py
@@ -71,7 +71,7 @@ def plot_pizza(player, df, metrics, league_avg):
         ),
 
         kwargs_params=dict(
-            fontsize=7.5,
+            fontsize=6,
             color="white",
             fontweight="bold"
         ),
