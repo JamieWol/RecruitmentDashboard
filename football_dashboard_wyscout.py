d2af184cd173412c7d846d86cbfbdf12a9ccac98
JamieWol <jamiewolstenholme99@hotmail.com>
2026-03-18 19:04:35 +0000

    Update football_dashboard_wyscout.py

--- football_dashboard_wyscout.py
+++ football_dashboard_wyscout.py
@@ -103,7 +103,7 @@ def plot_pizza(player, df, metrics, league_avg):
         fontsize=8,
         color="black",
         bbox=dict(
-            edgecolor="#3b82f6",
+            edgecolor="#black",
             facecolor="#3b82f6",
             boxstyle="round,pad=0.25"
         )
